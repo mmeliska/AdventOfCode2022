672d17e1287b50953ddc95229ec31ce30705bff4
Michael Meliska <mike@meliska.net>
2022-12-01 07:52:07 -0700

    Day 1

---
+++ dotnet/Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string content = File.ReadAllText("input.txt");
+
+var elfTotals = content.Split("\n\n")
+    .Select(elf => elf.Split('\n')
+        .Select(calCount => Int32.Parse(calCount))
+        .Sum()
+    );
+
+var topElfTotals = elfTotals.OrderByDescending(x => x).Take(3).ToList();
+
+Console.WriteLine("Top Elf: " +topElfTotals.First());
+Console.WriteLine("Sum of Top 3 Elves: " +topElfTotals.Sum());
