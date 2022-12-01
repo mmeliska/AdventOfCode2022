9f074853a73e91245795622c4bba1cc0f43055ea
Michael Meliska <mike@meliska.net>
2022-12-01 08:04:45 -0700

    Removing extra code

--- dotnet/Day1/Program.cs
+++ dotnet/Day1/Program.cs
@@ -4,11 +4,11 @@ string content = File.ReadAllText("input.txt");
 
 var elfTotals = content.Split("\n\n")
     .Select(elf => elf.Split('\n')
-        .Select(calCount => Int32.Parse(calCount))
+        .Select(Int32.Parse)
         .Sum()
     );
 
 var topElfTotals = elfTotals.OrderByDescending(x => x).Take(3).ToList();
 
-Console.WriteLine("Top Elf: " +topElfTotals.First());
-Console.WriteLine("Sum of Top 3 Elves: " +topElfTotals.Sum());
+Console.WriteLine("Top Elf: " + topElfTotals.First());
+Console.WriteLine("Sum of Top 3 Elves: " + topElfTotals.Sum());
