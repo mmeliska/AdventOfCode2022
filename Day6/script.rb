09cae2a5f053a4faa215f340e2939880a5474aad
Michael Meliska <mike@meliska.net>
2022-12-06 14:48:10 -0700

    Day6

--- Day6/script.rb
+++ Day6/script.rb
@@ -1,2 +1,32 @@
 # frozen_string_literal: true
+def calcSignalPosition(input, requiredUniqueChars)
+  lastChars = ""
+  input.each_char.with_index do |c, index|
+
+    indexOfChar = lastChars.index(c)
+
+    if indexOfChar != nil
+      lastChars = lastChars[indexOfChar + 1..-1] + c
+    elsif lastChars.length < requiredUniqueChars
+      lastChars += c
+    end
+
+    if lastChars.length == requiredUniqueChars
+      return index + 1
+    end
+
+  end
+
+  return lastChars.length == 14 ? input.length : -1
+end
+
+input = File.read("input.txt")
+
+# Part 1
+position1 = calcSignalPosition(input, 4)
+puts(position1)
+
+# Part 2
+position2 = calcSignalPosition(input, 14)
+puts(position2)
 
